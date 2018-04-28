b94188acc1c51cd22045dd2e597fd1465e36dc75
Artur Jorge <artur11jorge@gmail.com>
2018-04-28 23:56:09 +0100

    organizei e o server funciona (login, logout,...)

--- server.erl
+++ server.erl
@@ -1,76 +1,94 @@
-
-% TIRADO DA AULA PODE SER QUE ESTEJA TUDO ERRADO...
-
--module(login_manager).
+-module(server).
 -export([start/0, create_account/2, close_account/2, login/2, logout/1, online/0]).
 
-start() ->
-  pID = spawn(fun() -> loop(#{}) end),
-  register(?MODULE,Pid).
 
-create_account (User, Pass) -> ok | user_exists
-  ?MODULE ! {create_account User, Pass, self()},
-  receive {Res, ?MODULE} -> end.
-close_account  (User, Pass) -> ok | invalid
-  ?MODULE ! {close_account User, Pass, self()},
-  receive {Res, ?MODULE} -> end.
-login          (User, Pass) -> ok | invalid
-  ?MODULE ! {login  User, Pass, self()},
-  receive {Res, ?MODULE} -> end.
-logout         (User)       -> ok
-  ?MODULE ! {logout User,self()},
-  receive {Res, ?MODULE} -> end.
-%online         ()           -> [User]
-%  ?MODULE ! {online self()},
-%  receive {Res, ?MODULE} -> end.
+start() ->
+    Pid = spawn(fun() -> loop(#{}) end),
+    register(?MODULE, Pid). %MODULE? e tipo um define para o nome do modulo
+
+
+create_account(User, Pass) -> 
+    ?MODULE ! {create_account, User, Pass, self()},
+    receive {Res, ?MODULE} -> Res end.
+
+close_account(User, Pass) ->
+    ?MODULE ! {close_account, User, Pass, self()},
+    receive {Res, ?MODULE} -> Res end.
+
+login(User, Pass) ->
+    ?MODULE ! {login, User, Pass, self()},
+    receive {Res, ?MODULE} -> Res end.
+
+logout(User) ->
+    ?MODULE ! {logout, User, self()},
+    receive {Res, ?MODULE} -> Res end.
+
+online() ->
+    ?MODULE ! {online, self()},
+    receive {Res, ?MODULE} -> Res end.
 
 loop(Map) ->
-  receive
+    receive
+        {create_account, User, Pass, From} ->
+            case maps:find(User, Map) of
+                error ->
+                    From ! {ok, ?MODULE},
+                    loop(maps:put(User, {Pass, false}, Map));
+                _     ->
+                    From ! {user_exists, ?MODULE},
+                    loop(Map)
+            end;
 
-    {create_account, User, Pass, From} ->
-      case maps:find(User, Map) of
-        error ->
-          From ! {ok, ?MODULE},
-          loop(maps:put(User, {Pass, true}, Map));
-        _->
-          From ! {invalid, ?MODULE},
-          loop(Map)
-      end;
+        {close_account, User, Pass, From} ->
+            case maps:find(User, Map) of
+                error ->
+                    From ! {invalid_user, ?MODULE},
+                    loop(Map);
+                {ok, {Value, _}} ->
+                    if
+                        Value == Pass ->
+                            From ! {ok, ?MODULE},
+                            loop(maps:remove(User, Map));
+                        true ->
+                            From ! {invalid_pass, ?MODULE},
+                            loop(Map)
+                    end
+            end;
+        {login, User, Pass, From} ->
+            case maps:find(User, Map) of
+                error ->
+                    From ! {invalid_user, ?MODULE},
+                    loop(Map);
+                {ok, {Value, On}} ->
+                    if
+                        On ->
+                            From ! {already_On, ?MODULE},
+                            loop(Map);
+                        Value == Pass ->
+                            From ! {ok, ?MODULE},
+                            loop(maps:update(User, {Pass, true},  Map));
+                        true ->
+                            From ! {invalid_pass, ?MODULE},
+                            loop(Map)
+                    end
+            end;
+        {logout, User, From} ->
+            case maps:find(User, Map) of
+                error ->
+                    From ! {invalid_user, ?MODULE},
+                    loop(Map);
+                {ok, {Value, On}} ->
+                    if
+                        not On ->
+                            From ! {already_Off, ?MODULE},
+                            loop(Map);
+                        true ->
+                            From ! {ok, ?MODULE},
+                            loop(maps:update(User, {Value, false},  Map))
+                    end
+            end;
+        {online, From} ->
+            From ! {maps:keys(maps:filter(fun(_,V) -> erlang:element(2,V) end, Map)), ?MODULE},
+            loop(Map)
 
-    {close_account, User, Pass, From} ->
-      case maps:find(User, Map) of
-        {ok, {Pass, _}} ->
-          From ! {ok, ?MODULE},
-          loop(maps:remove(User, Map));
-        _->
-          From ! {invalid, ?MODULE},
-          loop(Map)
-      end;
-
-    {login, User, Pass, From} ->
-      case maps:find(User, Map) of
-        {ok, {Pass, _}} ->
-          % CONTADOR + 1
-          From ! {ok, ?MODULE},
-          loop(maps:login(User, Map));
-        _->
-          From ! {invalid, ?MODULE},
-          loop(Map)
-      end
-
-    {logout, User, From} ->
-      case maps:find(User, Map) of
-        {ok, {Pass, _}} ->
-          % CONTADOR - 1
-          From ! {ok, ?MODULE},
-          loop(maps:logout(User, Map));
-        _->
-          From ! {invalid, ?MODULE},
-          loop(Map)
-      end;
-
-    {online, From} ->
-
-      end;
-
-  end.
+    end.
